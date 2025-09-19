9251f69ce7318b38c7ba1f6ad3bc0de0e16431a9
mrunmayeehaha <deoremrunmayee1@gmail.com>
2025-09-19 15:26:23 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  
+</head>
+<body>
+    <script>
+       
+        let name = "Mrunmayee";
+        let age = 17;
+        let student = SY CSE;
+
+        
+        console.log("Name:", name);
+        console.log("Age:", age);
+        console.log("Is Student:", student);
+
+       
+        console.log("Type of myName:", typeof name);
+        console.log("Type of myAge:", typeof age);
+        console.log("Type of isStudent:", typeof student);
+    </script>
+
+</body>
+</html>
